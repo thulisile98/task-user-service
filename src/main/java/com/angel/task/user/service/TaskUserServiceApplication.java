dc0132f79223650d65e1040d96cfd2188fe257de
Angel Mashego <thulisile.ma98@gmail.com>
2024-01-31 10:55:25 +0200

    task-user-service and microservice service for task management system

---
+++ src/main/java/com/angel/task/user/service/TaskUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.angel.task.user.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskUserServiceApplication.class, args);
+	}
+
+}
